d4fe92433a29bef25d321e807a15b08bf3ee6427
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-25 15:32:57 +0200

    FIX

--- Front/src/components/About/Cursos/Cursos.tsx
+++ Front/src/components/About/Cursos/Cursos.tsx
@@ -4,13 +4,26 @@ import React from "react";
 
 const Cursos = () => {
   return (
-    <div className=" rounded bg-black/30 flex justify-center flex-col m-auto gap-4 p-8 transition-shadow duration-300 hover:shadow-lg hover:shadow-white/50">
+    <div className=" rounded bg-black/30 flex justify-center flex-col m-auto gap-4  transition-shadow duration-300 hover:shadow-lg hover:shadow-white/50">
       <div className="m-4 p-4 ">
         <h2 className="text-[30px] font-bold">Cursos</h2>
         <p className="text-[20px] text-darkD-300 font-medium">
           Graduado como desarrolador Full Stack, cursos de introduccion al
           desarrollo web, figma, github
         </p>
+      </div>
+      <div className="border ml-8 sm:w-[30%] md:w-[30%] lg:w-[40%] rounded-3xl  text-center m-4 transition-shadow duration-300 hover:shadow-lg hover:shadow-indigo-700/50">
+        <Link href="/cursos">
+          <div className="flex gap-4 justify-center items-center">
+            <p>Saber más</p>
+            <Image
+              width="30"
+              height="30"
+              src="/images/flecha.png"
+              alt="long-arrow-right"
+            />
+          </div>
+        </Link>
       </div>
 
       <div className="flex flex-wrap justify-center gap-8 items-center ">
@@ -32,19 +45,6 @@ const Cursos = () => {
             height={60}
           />
         </div>
-      </div>
-      <div className="border ml-8 sm:w-[30%] md:w-[30%] lg:w-[40%] rounded-3xl p-1 text-center m-4 transition-shadow duration-300 hover:shadow-lg hover:shadow-indigo-700/50">
-        <Link href="/cursos">
-          <div className="flex gap-4 justify-center items-center">
-            <p>Saber más</p>
-            <Image
-              width="30"
-              height="30"
-              src="/images/flecha.png"
-              alt="long-arrow-right"
-            />
-          </div>
-        </Link>
       </div>
     </div>
   );
