ae10bbfbdccbac8b8c58856db116ba61e8654592
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-25 15:33:27 +0200

    FIX

--- Front/src/components/magicui/Cloud/Cloud.tsx
+++ Front/src/components/magicui/Cloud/Cloud.tsx
@@ -35,7 +35,7 @@ const slugs = [
 
 export function IconCloudDemo() {
   return (
-    <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg px-20 pb-20 pt-8 ">
+    <div className="relative flex h-full w-full  items-center justify-center overflow-hidden rounded-lg m-1 ">
       <IconCloud iconSlugs={slugs} />
     </div>
   );
