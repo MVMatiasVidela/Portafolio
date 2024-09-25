dcf1be302882158478b74df411ca3a92ddbf5aff
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-25 14:24:58 +0200

    editar

--- Front/src/components/MaintenanceMode/MaintenancePage.tsx
+++ Front/src/components/MaintenanceMode/MaintenancePage.tsx
@@ -1,12 +1,22 @@
 
+import Image from 'next/image';
 import React from 'react';
 
 const MaintenancePage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen ">
-      <div className="text-center">
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <div className="bg-gradient-to-r from-purple-900  rounded-full w-[30%] border p-8 flex justify-center items-center">
+        <Image
+          className="flex justify-center items-center"
+          width={300}
+          height={300}
+          alt="Logo"
+          src="/images/logo.png"
+        />
+      </div>
+      <div className="text-center ">
         <h1 className="text-5xl font-bold text-white mb-4">
-          Sitio en Mantenimiento
+          🚧 Sitio en Mantenimiento 🔧
         </h1>
         <p className="text-xl text-gray-300">
           Estamos trabajando para mejorar su experiencia. Por favor, vuelva
