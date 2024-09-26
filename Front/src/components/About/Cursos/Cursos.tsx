384781e2487c939201d5dcca2fa3cdc1a2e83be4
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-26 16:28:40 +0200

    editions

--- Front/src/components/About/Cursos/Cursos.tsx
+++ Front/src/components/About/Cursos/Cursos.tsx
@@ -4,29 +4,29 @@ import React from "react";
 
 const Cursos = () => {
   return (
-    <div className=" rounded bg-black/30 flex justify-center flex-col m-auto gap-4  transition-shadow duration-300 hover:shadow-lg hover:shadow-white/50">
+    <div className=" max-h-[50em] rounded bg-black/30 flex justify-center flex-col m-auto gap-4  transition-shadow duration-300 hover:shadow-lg hover:shadow-white/50">
+      
       <div className="m-4 p-4 ">
         <h2 className="text-[30px] font-bold">Cursos</h2>
-        <p className="text-[20px] text-darkD-300 font-medium">
-          Graduado como desarrolador Full Stack, cursos de introduccion al
-          desarrollo web, figma, github
+        <p className="text-[20px] text-gray-300 font-medium">
+          He realizado diferentes tipos de cursos para el desarrollo web con los cuales he logrado reforzar mis conocimientos.
         </p>
-      </div>
-      <div className="border ml-8 sm:w-[30%] md:w-[30%] lg:w-[40%] rounded-3xl  text-center m-4 transition-shadow duration-300 hover:shadow-lg hover:shadow-indigo-700/50">
-        <Link href="/cursos">
-          <div className="flex gap-4 justify-center items-center">
-            <p>Saber más</p>
-            <Image
-              width="30"
-              height="30"
-              src="/images/flecha.png"
-              alt="long-arrow-right"
-            />
-          </div>
-        </Link>
+        <div className="border sm:w-[30%] md:w-[30%] lg:w-[40%] rounded-3xl p-1 text-center mt-4 transition-shadow duration-300 hover:shadow-lg hover:shadow-indigo-700/50">
+          <Link href="/cursos">
+            <div className="flex gap-4 justify-center items-center">
+              <p>Saber más</p>
+              <Image
+                width="30"
+                height="30"
+                src="/images/flecha.png"
+                alt="long-arrow-right"
+              />
+            </div>
+          </Link>
+        </div>
       </div>
 
-      <div className="flex flex-wrap justify-center gap-8 items-center ">
+      <div className="flex flex-wrap justify-center gap-8 items-center mb-8">
         <div className="hover:scale-125 transition duration-300">
           <Image
             className="rounded"
