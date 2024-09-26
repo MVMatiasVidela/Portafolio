2b2a1238b381c857f02efaf240f3377a455dbdbc
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-26 16:28:51 +0200

    p1

--- Front/src/components/About/Perfil/P1.tsx
+++ Front/src/components/About/Perfil/P1.tsx
@@ -6,13 +6,13 @@ const P1 = () => {
     <div className=" rounded bg-black/30 flex max-h-[50em] justify-center flex-col m-auto gap-4  transition-shadow duration-300 hover:shadow-lg hover:shadow-white/50">
       <div className="m-4 p-4 ">
         <h2 className="text-[30px] font-bold">Full stack developer</h2>
-        <p className="text-[20px] text-darkD-300 font-medium">
+        <p className="text-[20px] text-gray-300 font-medium">
           Desde principios de 2024 vengo aprendiendo del mundo de la
           programación, mi meta es seguir experimentando como desarrollador y
           adquirir valiosas experiencias.
         </p>
       </div>
-      <div className="justify-center items-center flex">
+      <div className="justify-center items-center flex mb-8">
         <Image
           className="rounded"
           src="/images/matias1.jpg"
