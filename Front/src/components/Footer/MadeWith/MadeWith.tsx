94113e5768f8a373757d40502f6f6c53b28e0fae
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-10-01 19:14:19 +0200

    -

--- Front/src/components/Footer/MadeWith/MadeWith.tsx
+++ Front/src/components/Footer/MadeWith/MadeWith.tsx
@@ -5,18 +5,25 @@ const MadeWith = () => {
   return (
     <div className="flex justify-center space-x-2 mt-8">
       <h1 className="text-white text-[20px] flex items-center">Hecho con:</h1>
-      <Image
-        width={40}
-        height={40}
-        src="https://img.icons8.com/fluency/48/000000/nextjs.png"
-        alt="nextjs"
-      />
-      <Image
-        width="40"
-        height="40"
-        src="https://img.icons8.com/fluency/48/tailwind_css.png"
-        alt="tailwind_css"
-      />
+      <div className="flex flex-col justify-center items-center p-4 hover:scale-125 transition duration-300">
+        <Image width="60" height="60" src="/images/next-js.svg" alt="nextjs" />
+      </div>
+      <div className="flex flex-col justify-center items-center p-4 hover:scale-125 transition duration-300">
+        <Image
+          width="40"
+          height="40"
+          src="https://img.icons8.com/fluency/48/tailwind_css.png"
+          alt="tailwind_css"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center p-4 hover:scale-125 transition duration-300">
+        <Image
+          width="70"
+          height="70"
+          src="/images/typescript.svg"
+          alt="typescript"
+        />
+      </div>
     </div>
   );
 };
