b0bd492e90e7cfecc12b9140a10624ea188a5e62
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-10-01 19:14:15 +0200

    -

--- Front/src/components/About/Perfil/P1.tsx
+++ Front/src/components/About/Perfil/P1.tsx
@@ -1,9 +1,11 @@
+
 import Image from 'next/image';
 import React from 'react'
 
 const P1 = () => {
   return (
     <div className=" rounded bg-black/30 flex max-h-[50em] justify-center flex-col m-auto gap-4  transition-shadow duration-300 hover:shadow-lg hover:shadow-white/50">
+      
       <div className="m-4 p-4 ">
         <h2 className="text-[30px] font-bold">Full stack developer</h2>
         <p className="text-[20px] text-gray-300 font-medium">
