65b40e1d04cdb09f66370872e30f8c1f574f451b
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-10-01 19:14:12 +0200

    -

--- Front/src/app/prueba/page.tsx
+++ Front/src/app/prueba/page.tsx
@@ -8,8 +8,9 @@ import { IconCloudDemo } from '@/components/magicui/Cloud/Cloud';
 import { AnimatedShinyTextDemo } from '@/components/magicui/Shiny/Shiny';
 import { BorderBeamDemo } from '@/components/magicui/Border/Border';
 import { NeonGradientCardDemo } from '@/components/magicui/Neon/Neon';
-import { BlurFadeTextDemo } from '@/components/magicui/blurFade/Blur';
+import { BlurFadeTextDemo } from '@/components/magicui/blurFade/BlurText';
 import { MagicCardDemo } from '@/components/magicui/magicCard/Card';
+import { BlurFadeDemo } from '@/components/magicui/blurFade/BlurCards';
 
 const page = () => {
   return (
@@ -19,10 +20,11 @@ const page = () => {
       <IconCloudDemo />
       <AnimatedShinyTextDemo />
       <BorderBeamDemo />
-      <NeonGradientCardDemo/>
-      <BlurFadeTextDemo/>
-      <MagicCardDemo/>
-
+      <NeonGradientCardDemo />
+      <BlurFadeTextDemo />
+      <MagicCardDemo />
+      <BlurFadeDemo />
+      
     </div>
   );
 }
